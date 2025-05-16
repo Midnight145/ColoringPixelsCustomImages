aff1cf5abb212a8cbf1ecb85f980f97863126d19
Midnight <23142346+Midnight145@users.noreply.github.com>
2025-05-16 17:41:09 -0500

    major refactor, multithread everywhere possible

--- src/ColoringPixelsPlugin/Custom/ImageLoader.cs
+++ src/ColoringPixelsPlugin/Custom/ImageLoader.cs
@@ -13,7 +13,7 @@ public static class ImageLoader {
     public static LevelData[] levels;
 
     private static readonly ManualLogSource Logger = CustomImagesPlugin.Log;
-    private static bool initialized = false;
+    private static bool initialized;
 
     public static void Init() {
         if (initialized) {
@@ -21,7 +21,8 @@ public static class ImageLoader {
         }
         ImageLoader.Logger.LogInfo("Loading Custom Levels");
         string path = Path.Combine(Application.persistentDataPath, "Custom");
-        var levelList = new List<LevelData>();
+        var levelList = new ConcurrentBag<LevelData>();
+
         int index = 0;
         string[] pngFiles = Directory.GetFiles(path, "*.png");
 
@@ -33,15 +34,12 @@ public static class ImageLoader {
                 fileMap.Add(pngFile, binFile);
             }
         }
-        
         List<string> uncached = pngFiles
             .Where(png => !fileMap.ContainsKey(png))
             .ToList();
-        
-//          Name, Colors, Width, Height, Sprite
-        var data = new ConcurrentDictionary<string, Tuple<string, int, int, int, Sprite>>();
-        var pixels = new ConcurrentDictionary<string, Color32[]>();
-        var quantizedPixels = new ConcurrentDictionary<string, Color32[]>();
+
+//          Name, Colors, Width, Height, Sprite, Texture
+        var data = new ConcurrentDictionary<string, Tuple<string, int, int, int, Sprite, Texture2D>>();
         const string pattern = @"^.+/(?<name>.+?)_(?<dimension>\d+?)_(?<colors>\d+)\.png$";
         Regex regex = new Regex(pattern);
 
@@ -53,14 +51,14 @@ public static class ImageLoader {
             }
             Texture2D texture = new Texture2D(2, 2); // temp size
             bool isLoaded = texture.LoadImage(fileData); // resized automatically
-            
+
             if (!isLoaded) {
                 Logger.LogError($"Failed to load image data into texture for {file}");
                 continue;
             }
-            
+
             Logger.LogDebug("Processing file " + file);
-            
+
             Match match = regex.Match(file);
             string name;
             int dimension, colors;
@@ -70,53 +68,54 @@ public static class ImageLoader {
                 colors = int.Parse(match.Groups["colors"].Value);
             } else {
                 name = file;
-                dimension = 100;
-                colors = 25;
+                dimension = Math.Max(texture.width, texture.height);
+                colors = 99;
             }
-            Logger.LogDebug("Matched " + name + " " + dimension + " " + colors);
-            
+
             Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
             int width = texture.width;
             int height = texture.height;
-            
+            Texture2D qtexture = null;
             if (!fileMap.ContainsKey(file)) {
                 // Resize the image if necessary
                 // Used to later make the level data, but that's not needed if loading from cache.
                 Texture2D resizedImage = ImageProcessor.ResizeImage(texture, dimension);
-                pixels[file] = resizedImage.GetPixels32();
                 Logger.LogDebug("Resized image to " + resizedImage.width + "x" + resizedImage.height);
                 width = resizedImage.width;
                 height = resizedImage.height;
+                qtexture = resizedImage;
             }
-            
-            data[file] = Tuple.Create(name, colors, width, height, sprite);
+
+            data[file] = Tuple.Create(name, colors, width, height, sprite, qtexture);
+
         }
 
-        // we can only do so much in parallel due to thread safety checks in Unity
-        // unfortunately, this means that we have to do this in two passes and iterate over pngFiles twice
-        
-        // parallelizing this is worth the performance hit though, especially when you have several large images
-        Parallel.ForEach(uncached, file => {
-            Logger.LogDebug("Starting quantization for " + data[file].Item1);
-            quantizedPixels[file] = ImageProcessor.QuantizeImage(pixels[file], data[file].Item2);
-            Logger.LogDebug("Quantized image for " + data[file].Item1);
-        });
-
-        foreach (string file in pngFiles) {
-            short[] levelData;
+        Parallel.ForEach(pngFiles, file => {
             string name = data[file].Item1;
+            int colors = data[file].Item2, width = data[file].Item3, height = data[file].Item4;
             Sprite sprite = data[file].Item5;
+            Texture2D qtexture = data[file].Item6;
+            Logger.LogInfo($"Loading {name} ({width}x{height}) with {colors} colors");
+
+
+            if (uncached.Contains(file)) {
+                Logger.LogDebug("Starting quantization for " + name);
+                Color32[] quantizedPixels = ImageProcessor.QuantizeImage(qtexture.GetPixels32(), colors);
+                qtexture.SetPixels32(quantizedPixels);
+                Logger.LogDebug("Quantized image for " + name);
+            }
+            short[] levelData;
+
             if (!fileMap.TryGetValue(file, out string value)) {
-                levelData = CreateLevelData(data[file].Item3, data[file].Item4, quantizedPixels[file]);
+                levelData = CreateLevelData(qtexture);
                 CreateCache(path, file, levelData);
             }
             else {
                 byte[] buffer = File.ReadAllBytes(value);
                 levelData = new short[buffer.Length / 2];
                 Buffer.BlockCopy(buffer, 0, levelData, 0, buffer.Length);
-                CreateImageSprites(levelData);
+                // CreateImageSprites(levelData);
             }
-            
             index -= 1;
             LevelData level = new LevelData(new[] { name, "Custom", "" }, index, null) {
                 fullLevelData = levelData.ToArray(),
@@ -124,26 +123,22 @@ public static class ImageLoader {
                 levelSprite = sprite
             };
             levelList.Add(level);
-        }
-
+        });
+        int maxColorCount = data.Values.Max(t => t.Item2);
+        CreateImageSprites(maxColorCount);
         levels = levelList.ToArray();
+
         initialized = true;
-        
-        // clear the dictionaries to free up memory
-        quantizedPixels.Clear();
-        pixels.Clear();
-        data.Clear();
+
+        data.Clear(); // free up memory
     }
 
-    private static short[] CreateLevelData(int width, int height, Color32[] pixels) {
-        Texture2D quantizedImage = new Texture2D(width, height);
-        quantizedImage.SetPixels32(pixels);
-        quantizedImage.Apply();
-        Logger.LogDebug("Extracting colors...");
-        List<(int, int, int)> colors = LevelDataCreator.ExtractColors(quantizedImage);
+    private static short[] CreateLevelData(Texture2D quantizedImage) {
+        List<(short, short, short)> colors = LevelDataCreator.ExtractColors(quantizedImage);
+        Logger.LogDebug("Extracted " + colors.Count + " colors");
         LevelDataCreator creator = new LevelDataCreator(quantizedImage, colors);
 
-        return creator.CreateLevelData().Select(item => (short)item).ToArray();
+        return creator.CreateLevelData().ToArray();
     }
     
     private static void CreateCache(string path, string file, short[] levelData) {
@@ -157,11 +152,11 @@ public static class ImageLoader {
         }
     }
 
-    private static void CreateImageSprites(short[] levelData) {
-        int colorCount = levelData[2 + levelData[0] * levelData[1]];
-        if (colorCount >= 99) {
-            if (CustomSprites.sprites.Count < colorCount - 99) {
-                for (int i = 0; i < colorCount - 99; i++) {
+    private static void CreateImageSprites(int count) {
+        ImageLoader.Logger.LogInfo("Creating image sprites for " + count);
+        if (count >= 99) {
+            if (CustomSprites.sprites.Count < count - 99) {
+                for (int i = 0; i < count - 99; i++) {
                     CustomSprites.CreateSprite(i);
                 }
             }
@@ -171,30 +166,20 @@ public static class ImageLoader {
 
 internal class LevelDataCreator {
     private readonly Texture2D image;
-    private readonly List<int> fullLevelData;
-    private readonly List<(int, int, int)> colors;
+    private readonly List<short> fullLevelData;
+    private readonly List<(short, short, short)> colors;
 
-    public LevelDataCreator(Texture2D image, List<(int, int, int)> colors) {
+    public LevelDataCreator(Texture2D image, List<(short, short, short)> colors) {
         this.image = image;
         this.colors = colors;
-        this.CreateNewSprites();
-        this.fullLevelData = new List<int>();
+        // this.CreateNewSprites();
+        this.fullLevelData = new List<short>();
     }
 
-    private void CreateNewSprites() {
-        if (this.colors.Count >= 99) {
-            if (CustomSprites.sprites.Count < this.colors.Count - 99) {
-                for (int i = 0; i < this.colors.Count - 99; i++) {
-                    CustomSprites.CreateSprite(i);
-                }
-            }
-        }
-    }
-
-    public List<int> CreateLevelData() {
+    public List<short> CreateLevelData() {
         // Add width, height, and initial padding
-        fullLevelData.Add(image.width);
-        fullLevelData.Add(image.height);
+        fullLevelData.Add((short) image.width);
+        fullLevelData.Add((short) image.height);
 
         // Loop through each pixel in the image
         for (int x = 0; x < image.width; x++) {
@@ -202,21 +187,21 @@ internal class LevelDataCreator {
                 int flippedY = image.height - 1 - y;
                 // Get the pixel color at the given position
                 Color32 pixel = image.GetPixel(x, flippedY);
-                int r = pixel.r;
-                int g = pixel.g;
-                int b = pixel.b;
-                int a = pixel.a;
+                short r = pixel.r;
+                short g = pixel.g;
+                short b = pixel.b;
+                short a = pixel.a;
 
                 // Append color index or 0 for transparency
-                fullLevelData.Add(a == 0 ? 0 : colors.IndexOf((r, g, b)) + 1);
+                fullLevelData.Add((short)(a == 0 ? 0 : this.colors.IndexOf((r, g, b)) + 1));
             }
         }
 
         // Add the color palette length and padding
-        fullLevelData.Add(colors.Count);
+        fullLevelData.Add((short) colors.Count);
 
         // Append each color component with padding
-        foreach ((int, int, int) color in colors) {
+        foreach ((short, short, short) color in colors) {
             fullLevelData.Add(color.Item1);
             fullLevelData.Add(color.Item2);
             fullLevelData.Add(color.Item3);
@@ -225,11 +210,11 @@ internal class LevelDataCreator {
         return fullLevelData;
     }
     
-    public static List<(int, int, int)> ExtractColors(Texture2D texture)
+    public static List<(short, short, short)> ExtractColors(Texture2D texture)
     {
         return texture.GetPixels32()
             .Where(p => p.a != 0)
-            .Select(p => ((int) p.r, (int) p.g, (int) p.b))
+            .Select(p => ((short) p.r, (short) p.g, (short) p.b)).Distinct()
             .ToList();
     }
 }
