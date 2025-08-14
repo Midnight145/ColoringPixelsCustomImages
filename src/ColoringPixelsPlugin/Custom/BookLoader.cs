6cb0b4111f0c550050107e4115720fb8aabf7097
Midnight <23142346+Midnight145@users.noreply.github.com>
2025-08-14 16:14:37 -0500

    publicize assembly in favor of R

--- src/ColoringPixelsPlugin/Custom/BookLoader.cs
+++ src/ColoringPixelsPlugin/Custom/BookLoader.cs
@@ -23,23 +23,17 @@ namespace ColoringPixelsPlugin.Custom {
 
         public static BookDetails[] AddCustomBook() {
             ImageLoader.Initialize();
-            R allBookDetails = R.of(typeof(AllBookDetails));
-            // we have to use private inst_ here because inst is a property
-            // if we use the public inst we recur forever because this method is called from the property getter
-            var inst = allBookDetails.GetField("_inst") as BookDetails[];
-
             foreach (CustomBook book in CustomBook.books) {
-                AddBook(ref inst, book);
-            }
+                AddBook(ref AllBookDetails._inst, book);
 
-            allBookDetails.SetField("_inst", inst);
-            return inst;
+            }
+            return AllBookDetails._inst;
         }
 
+        // ReSharper disable once UnusedMember.Global (called via transpiler)
         public static void AddSection() {
             MainMenuBookSpawner mainMenuBookSpawner = BookLoader.FindObjectOfType<MainMenuBookSpawner>();
-            R.of(mainMenuBookSpawner).CallMethod("AddSection",
-                CustomBook.books.Select(book => book.GetBookDetails()).ToArray(), "Custom");
+            mainMenuBookSpawner.AddSection(CustomBook.books.Select(book => book.GetBookDetails()).ToArray(), "Custom");
         }
     }
 }
